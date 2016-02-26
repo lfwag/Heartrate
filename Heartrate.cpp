026ac219f77e3426f998579b35f094355c099f30
lfwag <490289303@qq.com>
2016-02-26 14:23:06 +0800

    Update Heartrate.cpp

--- Heartrate.cpp
+++ Heartrate.cpp
@@ -88,6 +88,7 @@ void Heartrate::minNumber(unsigned char count)
 
 unsigned int Heartrate::analogGetRate(void)
 {
+
 	static unsigned char timeFlag;
 	static unsigned long sampleTime[10];
 	unsigned long valueTime_;
@@ -95,16 +96,64 @@ unsigned int Heartrate::analogGetRate(void)
 	if(maxNumber(valueCount_))
 	{
 		sampleTime[timeFlag]=millis();
-		if(timeFlag < 9){			
-			valueTime_ = sampleTime[timeFlag];
-			valueTime_ -=  sampleTime[++timeFlag];
-			valueTime_ = 540000/valueTime_;	
-			return((unsigned int)valueTime_);
+		
+		unsigned char timeFlagMax = timeFlag;
+		unsigned char timeFlagMin;
+		if(timeFlag){
+			timeFlagMin = timeFlag-1;
 		}else{
-			valueTime_=540000/(sampleTime[timeFlag]-sampleTime[0]);
-			timeFlag = 0;
-			return((unsigned int)valueTime_);
+			timeFlagMin = 9;
+		}
+		for(int i=0;i<9;i++){
+			if((sampleTime[timeFlagMax]-sampleTime[timeFlagMin])<270 || (sampleTime[timeFlagMax]-sampleTime[timeFlagMin])>2000){
+				timeFlag++;
+				if(timeFlag>=10)timeFlag=0;
+				return(0);
+			}
+			if(timeFlagMax){
+				timeFlagMax--;
+			}else{
+				timeFlagMax = 9;
+			}
+			if(timeFlagMin){
+				timeFlagMin--;
+			}else{
+				timeFlagMin = 9;
+			}				
+		}
+		
+		
+		unsigned long Arrange[9]={0};
+		unsigned char timeFlag_ = timeFlag++;
+		for(int i=0;i<9;i++){
+			if(timeFlag_){	
+				Arrange[i] = sampleTime[timeFlag_]-sampleTime[timeFlag_-1];	
+				timeFlag_--;				
+			}else{
+				Arrange[i] = sampleTime[0]-sampleTime[9];
+				timeFlag_ = 9;
+			}						
+		}
+		unsigned long Arrange_=0;
+		for(int i=8;i>0;i--){
+			for(int j=0;j<i;j++)
+			if(Arrange[j]>Arrange[j+1]){
+				Arrange_ = Arrange[j];
+				Arrange[j] = Arrange[j+1];
+				Arrange[j+1] = Arrange_;
+			}
+		}
+		Arrange_ = 0;
+		for(int i=2;i<=6;i++){
+			Arrange_ += Arrange[i];
 		}		
+
+		if((Arrange[6]-Arrange[2])>100){
+			return(0);
+		}	
+		
+		valueTime_ = 300000/Arrange_;///<60*1000*(6-1)	
+		return((unsigned int)valueTime_);
 	}	
 	return(0);
 }
@@ -119,6 +168,7 @@ unsigned int Heartrate::analogGetRate(void)
 
 unsigned int Heartrate::digitalGetRate(void)
 {
+
 	static unsigned char timeFlag;
 	static unsigned long sampleTime[10];
 	unsigned long valueTime_;
@@ -130,18 +180,65 @@ unsigned int Heartrate::digitalGetRate(void)
 		count_ = SAMPLE_NUMBER-1;
 	}
 	if((value[valueCount_]>1000)&&(value[count_]<20)){
-		sampleTime[timeFlag]=millis();
-		if(timeFlag < 9){			
-			valueTime_ = sampleTime[timeFlag];
-			valueTime_ -=  sampleTime[++timeFlag];
-			valueTime_ = 540000/valueTime_;	
-			return((unsigned int)valueTime_);
+		sampleTime[timeFlag]=millis();	
+		
+		unsigned char timeFlagMax = timeFlag;
+		unsigned char timeFlagMin;
+		if(timeFlag){
+			timeFlagMin = timeFlag-1;
 		}else{
-			valueTime_=540000/(sampleTime[timeFlag]-sampleTime[0]);
-			timeFlag = 0;
-
-			return((unsigned int)valueTime_);
+			timeFlagMin = 9;
 		}
+		for(int i=0;i<9;i++){
+			if((sampleTime[timeFlagMax]-sampleTime[timeFlagMin])<270 || (sampleTime[timeFlagMax]-sampleTime[timeFlagMin])>2000){
+				timeFlag++;
+				if(timeFlag>=10)timeFlag=0;
+				return(0);
+			}
+			if(timeFlagMax){
+				timeFlagMax--;
+			}else{
+				timeFlagMax = 9;
+			}
+			if(timeFlagMin){
+				timeFlagMin--;
+			}else{
+				timeFlagMin = 9;
+			}				
+		}
+		
+		unsigned long Arrange[9]={0};
+		unsigned char timeFlag_ = timeFlag++;
+		for(int i=0;i<9;i++){
+			if(timeFlag_){	
+				Arrange[i] = sampleTime[timeFlag_]-sampleTime[timeFlag_-1];	
+				timeFlag_--;				
+			}else{
+				Arrange[i] = sampleTime[0]-sampleTime[9];
+				timeFlag_ = 9;
+			}						
+		}
+		unsigned long Arrange_=0;
+		for(int i=8;i>0;i--){
+			for(int j=0;j<i;j++)
+			if(Arrange[j]>Arrange[j+1]){
+				Arrange_ = Arrange[j];
+				Arrange[j] = Arrange[j+1];
+				Arrange[j+1] = Arrange_;
+			}
+		}
+		Arrange_ = 0;
+		for(int i=2;i<=6;i++){
+			Arrange_ += Arrange[i];
+		}		
+	
+		if((Arrange[6]-Arrange[2])>100){
+			return(0);
+		}	
+		
+		valueTime_ = 300000/Arrange_;///<60*1000*(6-1)
+		return((unsigned int)valueTime_);
+
 	}else{
 		return(0);
 	}
